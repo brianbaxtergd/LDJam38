37a21d58a4aaf25f5b0f161b2c98a7f8446342ac
Nicholas <nbgilden@fullsail.edu>
2017-04-22 19:29:18 -0400

    Added obstacle redirect

--- Assets/Scripts/ObstacleBehavior.cs
+++ Assets/Scripts/ObstacleBehavior.cs
@@ -6,25 +6,27 @@ public class ObstacleBehavior : MonoBehaviour
 {
 	Rigidbody RB;
 	[SerializeField]
-	float DestoryDistance = 6.0f;//Positive is behind player
+	float DestoryDistance = 6.0f;
 
 	Vector3 originalPos;
 
 	public void Go(float speed, Vector3 direction)
 	{
 		originalPos = transform.position;
-		RB = GetComponent<Rigidbody>();
+		if(RB == null)
+			RB = GetComponent<Rigidbody>();
+		RB.velocity = Vector3.zero;
 		Vector3 vel = direction * speed;
 		RB.velocity = vel;
 	}
 
 	void Update()
 	{
-		Vector3 pos = transform.position - originalPos;
+		/*Vector3 pos = transform.position - originalPos;
 		if(Vector3.Dot(pos,pos) > DestoryDistance * DestoryDistance)
 		{
 			//Destory or reset
 			Destroy(gameObject);
-		}
+		}*/
 	}
 }
---
+++ Assets/Scripts/ObstacleReDirectScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleReDirectScript : MonoBehaviour 
+{
+	[SerializeField]
+	float NewSpeed = 10.0f;
+	[SerializeField]
+	Vector3 NewDirection = Vector3.zero;
+	[SerializeField]
+	bool RotateToDirection = false;
+
+	void OnTriggerEnter(Collider col)
+	{
+		col.GetComponent<ObstacleBehavior>().Go(NewSpeed, NewDirection);
+		if(RotateToDirection)
+		{
+			Vector3 oldForward = col.transform.forward;
+			float angle = Vector3.Angle(oldForward, NewDirection);
+			col.transform.Rotate(Vector3.up, -angle, Space.World);
+		}
+	}
+}
