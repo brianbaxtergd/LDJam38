52d23109b6deb3379536b05aaa6593ad52ee09f7
Nicholas <nbgilden@fullsail.edu>
2017-04-22 01:52:21 -0400

    No random speed Added more spawners to test random spawning

--- Assets/Scripts/SpawnerGodBehavior.cs
+++ Assets/Scripts/SpawnerGodBehavior.cs
@@ -6,30 +6,39 @@ public class SpawnerGodBehavior : MonoBehaviour
 {
 	[SerializeField]
 	GameObject[] Obstacles = null;
+	[SerializeField]
+	float ObstacleSpeed = 10.0f;
 
-	//This should be per spawner
-	float spawnRate = 1.0f;
-	float timer = 0.0f;
+
+	float[] SpawnRates = null;
+	float[] Timers = null;
 
 	SpawnerBehavior[] childSpawners;
 
 	void Start()
 	{
 		childSpawners = GetComponentsInChildren<SpawnerBehavior>();
+		SpawnRates = new float[childSpawners.Length];
+		Timers = new float[childSpawners.Length];
+		for(int i = 0; i < childSpawners.Length; ++i)
+		{
+			Timers[i] = 0.0f;
+			SpawnRates[i] = Random.Range(0.5f, 3.0f);
+		}
 	}
 
-	void Update () 
+	void Update ()
 	{
-		timer += Time.deltaTime;
-		if(timer > spawnRate)
+		for(int i = 0; i < Timers.Length; ++ i)
 		{
-			foreach(var child in childSpawners)
+			Timers[i] += Time.deltaTime;
+			if(Timers[i] > SpawnRates[i])
 			{
 				int obstacle = Random.Range(0, Obstacles.Length);
-				child.SpawnObstacle(Obstacles[obstacle], Random.Range(7.0f,15.0f));
+				childSpawners[i].SpawnObstacle(Obstacles[obstacle], ObstacleSpeed);
+				Timers[i] = 0.0f;
+				SpawnRates[i] = Random.Range(0.75f, 3.0f);
 			}
-			timer = 0.0f;
-			spawnRate = Random.Range(0.75f, 3.0f);
 		}
 	}
 }
