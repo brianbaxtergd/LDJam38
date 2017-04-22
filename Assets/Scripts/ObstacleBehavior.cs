e48ee75e625cff142ec50838e7f9c6a690b07569
Nicholas <nbgilden@fullsail.edu>
2017-04-22 15:37:07 -0400

    changed some script values added ability for obstacles to move a different direction

--- Assets/Scripts/ObstacleBehavior.cs
+++ Assets/Scripts/ObstacleBehavior.cs
@@ -6,10 +6,13 @@ public class ObstacleBehavior : MonoBehaviour
 {
 	Rigidbody RB;
 	[SerializeField]
-	float DestoryPoint = 6.0f;//Positive is behind player
+	float DestoryDistance = 6.0f;//Positive is behind player
+
+	Vector3 originalPos;
 
 	public void Go(float speed, Vector3 direction)
 	{
+		originalPos = transform.position;
 		RB = GetComponent<Rigidbody>();
 		Vector3 vel = direction * speed;
 		RB.velocity = vel;
@@ -17,7 +20,8 @@ public class ObstacleBehavior : MonoBehaviour
 
 	void Update()
 	{
-		if(transform.position.x > DestoryPoint)
+		Vector3 pos = transform.position - originalPos;
+		if(Vector3.Dot(pos,pos) > DestoryDistance * DestoryDistance)
 		{
 			//Destory or reset
 			Destroy(gameObject);
